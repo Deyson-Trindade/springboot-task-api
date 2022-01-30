96f88653d250edf36d333c303e33257c74051f74
Deyson <deyson.trindade@smilego.com.br>
2022-01-30 20:00:32 -0300

    hot-fix, remembered to annotate @CreationTimestamp and @UpdateTimestamp on model chore

--- src/main/java/com/notes/app/model/Chore.java
+++ src/main/java/com/notes/app/model/Chore.java
@@ -2,6 +2,7 @@ package com.notes.app.model;
 
 import lombok.Data;
 import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
 
 import javax.persistence.*;
 import java.sql.Timestamp;
@@ -25,10 +26,11 @@ public class Chore {
     @Column(name = "finished", nullable = false)
     private boolean finished;
 
+    @CreationTimestamp
     @Column(name = "created_at", nullable = false)
     private Timestamp created_at;
 
-    @CreationTimestamp
+    @UpdateTimestamp
     @Column(name = "updated_at", nullable = true)
     private Timestamp updated_at;
 
